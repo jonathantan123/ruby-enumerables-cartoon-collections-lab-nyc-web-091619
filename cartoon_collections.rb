e599eb625fcf7168e68b6c5848cbbb52290fc16a
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-05 15:29:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,12 +15,9 @@ def long_planeteer_calls (array)
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese (array)
-   cheese_types = ["cheddar", "gouda", "camembert"]
-  if  array.find { |x| 
-    cheese_types.include?(x)
-    puts yay 
-  else 
-    nil 
-  end 
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
+
