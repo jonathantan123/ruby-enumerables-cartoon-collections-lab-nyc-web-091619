f0153655aee76a543a65c855dd2b080453d0f3dc
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-05 15:27:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese (array)
    cheese_types = ["cheddar", "gouda", "camembert"]
-  if  array.find { |x| 
+  if  array.find { |x|
     cheese_types.include?(x)
   else 
     nil 
