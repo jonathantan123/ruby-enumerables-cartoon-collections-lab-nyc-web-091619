cb21b360a659578b366744f10c924d02ab98ef1b
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-05 15:22:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves) 
+ dwarves = ["Dopey", "Grumpy", "Bashful"]
+ 
+ 
+  dwarves.each_with_index {|dwarves, index| puts "#{index+1} #{dwarves}"}
+  
+ 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  array.map{|element| element.capitalize + "!"}
+end 
+
+def long_planeteer_calls (array)
+  array.any? {|element| element.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese (array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+  if  array.include? {|element| 
+    puts yay 
+  else 
+    nil 
+  end 
 end
